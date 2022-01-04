d446dfa5da3622baa8a1c0eb92b1562d8338d5d3
Jeffrey Widmer <jeffrey.widmer@gyso.ch>
2022-01-04 16:06:10 +0100

    update

---
+++ page-lieferservice.php
@@ -0,0 +1,14 @@
+<!-- Include Header -->
+<?php include './includes/header.php'; ?>
+<div id="page-lieferservice">
+<!-- || START HTML -->
+<!-- ||@ Content / HTML -->
+
+
+
+
+
+<!-- || ENDE HTML -->
+</div>
+<!-- Include Footer -->
+<?php include './includes/footer.php'; ?>
